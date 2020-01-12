b5188ccbe54b8555422050b022c3821120d33066
George Blackmore <gtblackmore@gmail.com>
2020-01-12 18:12:09 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,22 @@
+class Artist
+  attr_accessor :name
+ 
+  @@all = []
+ 
+  def initialize(name)
+    @name = name
+    @@all << name
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def add_song(song_name)
+    song_name = Song.new
+  
+   
+
+
+
+end
